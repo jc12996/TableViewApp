53af3ac5c0120baed3794c9d8eb30bc8d74af7cf
John Christiansen <johnchristiansen@Johns-MacBook-Pro.local>
2014-08-04 18:41:33 -0600

    Initial Commit

---
+++ KingsleyManagementCorpApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KingsleyManagementCorpApp
+//
+//  Created by John Christiansen on 8/4/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KingsleyManagementCorpApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KingsleyManagementCorpApp
+//
+//  Created by John Christiansen on 8/4/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
